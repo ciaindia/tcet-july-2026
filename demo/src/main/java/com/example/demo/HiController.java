87782e1d9d10d13d5a4c4a33125c11a8640bd458
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-07-04 12:28:54 +0530

    post with body

--- demo/src/main/java/com/example/demo/HiController.java
+++ demo/src/main/java/com/example/demo/HiController.java
@@ -1,6 +1,10 @@
 package com.example.demo;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,9 +27,19 @@ public class HiController {
         return "I am from GET calc";
     }
 
+    //Add 2 numbers using post num1 and num2 payload
+    //    http://localhost:9090/calc?num1=5&num2=6
+
     @PostMapping("/calc")
-    public String calcPost() {
-        return "I am from POST calc";
+    public String calcPost(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return String.valueOf(num1 + num2);
+    }    
+
+    @PostMapping("/sqr") 
+    public String getSqr(@RequestBody Map<String, Integer> body)
+    {
+        int num = body.get("num");
+        return String.valueOf(num*num);
     }
-    
+
 }
