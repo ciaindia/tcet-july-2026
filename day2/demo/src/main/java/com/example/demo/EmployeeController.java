9139214b9baea10eac746e634487ecb5365c7f2f
Shailesh Sonare <shailesh.sonare@netcorecloud.com>
2026-07-11 11:20:00 +0530

    employeepost

--- day2/demo/src/main/java/com/example/demo/EmployeeController.java
+++ day2/demo/src/main/java/com/example/demo/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 class Employee {
@@ -64,5 +66,12 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return emps;
     } 
+
+    //http://localhost:9090/employees 
+    //body: { "eid": 4, "name": "ddd", "department": "HR", "salary": 40000 }
+    @PostMapping("/employees")
+    public void addEmployee(@RequestBody Employee employee) {
+        emps.add(employee);
+    }   
     
 }
